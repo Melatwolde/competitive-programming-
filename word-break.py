f81083b2329e6861224a2a4f5e803e33d7e3a2ab
Melat woldegiorgis <123128037+Melatwolde@users.noreply.github.com>
2023-08-05 00:25:24 +0300

    Create word-break.py

---
+++ word-break.py
@@ -0,0 +1,17 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+
+        max_len = 0
+        for string in wordDict:
+            max_len = max(max_len, len(string))
+
+        can_break = [False for _ in range(n + 1)]
+        can_break[0] = True
+        for i in range(1, n + 1):
+            for l in range(1, min(i, max_len) + 1):
+                if can_break[i-l] and s[i-l:i] in wordDict:
+                    can_break[i] = True
+                    break
+
+        return can_break[-1]
