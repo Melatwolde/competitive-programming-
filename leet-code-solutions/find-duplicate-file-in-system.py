83be7aea3c86dd3a9f8e65d29f8efd424b152431
Melat woldegiorgis <123128037+Melatwolde@users.noreply.github.com>
2023-12-16 21:53:46 +0300

    Added solution for Find Duplicate File in System

---
+++ leet-code-solutions/find-duplicate-file-in-system.py
@@ -0,0 +1,20 @@
+
+class Solution:
+    def findDuplicate(self, paths: List[str]) -> List[List[str]]:
+        contents = defaultdict(list)
+        res = list()
+        for direc in paths:
+            d = direc.split()
+            for i in range(1, len(d)):
+                x = d[i].split("(")
+                cont = x[1][:-1]
+                file_name = d[0] + "/" + x[0]
+                contents[cont].append(file_name)
+                
+        for file in contents:
+            if len(contents[file]) > 1:
+                res.append(contents[file])
+                
+        return res
+                
+            
