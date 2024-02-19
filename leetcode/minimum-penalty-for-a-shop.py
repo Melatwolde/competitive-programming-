503905703b5cc99d628bfb37f256984f2ea62faa
Melat woldegiorgis <123128037+Melatwolde@users.noreply.github.com>
2024-02-19 23:56:01 +0300

    Add solution for Minimum Penalty for a Shop

---
+++ leetcode/minimum-penalty-for-a-shop.py
@@ -0,0 +1,13 @@
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        cur = total = customers.count('Y')
+        ans = 0
+
+        for j in range(len(customers)):
+            total += 1 if customers[j] == 'N' else -1
+            
+            if total < cur:
+                cur = total
+                ans = j + 1
+
+        return ans
